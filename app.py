7cba5e1b4fb4ee6faba54275b0b6bfeba4f59818
gaur1857 <gaur.1857@gmail.com>
2026-04-17 16:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,84 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "secret"
+app.secret_key = "secret123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-# fake database
-users = []
-tickets = []
+db = SQLAlchemy(app)
 
-# Home page (PROTECTED)
+# USER MODEL
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    role = db.Column(db.String(10))  # admin/user
+
+# TICKET MODEL
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    issue = db.Column(db.String(200))
+
 @app.route('/')
 def home():
-    if 'user_id' not in session:
-        return redirect('/login')
     return render_template('index.html')
 
-# Signup
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        user = {
-            "id": len(users) + 1,
-            "username": request.form['username'],
-            "password": request.form['password']
-        }
-        users.append(user)
-        return redirect('/login')
-    return render_template('signup.html')
-
-# Login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        for user in users:
-            if user['username'] == request.form['username'] and user['password'] == request.form['password']:
-                session['user_id'] = user['id']
-                return redirect('/')
-        return "Invalid username or password"   # helpful debug
-    return render_template('login.html')
-
-# Logout (NEW)
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect('/login')
-
-# Add Ticket
 @app.route('/add', methods=['POST'])
 def add_ticket():
-    if 'user_id' not in session:
-        return redirect('/login')
-
-    ticket = {
-        "name": request.form['name'],
-        "issue": request.form['issue'],
-        "user_id": session['user_id']
-    }
-    tickets.append(ticket)
-
+    ticket = Ticket(name=request.form['name'], issue=request.form['issue'])
+    db.session.add(ticket)
+    db.session.commit()
     return redirect('/tickets')
 
-# Show only user's tickets
 @app.route('/tickets')
-def view_tickets():
-    if 'user_id' not in session:
+def tickets():
+    data = Ticket.query.all()
+    return str([(t.name, t.issue) for t in data])
+
+# LOGIN
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(
+            username=request.form['username'],
+            password=request.form['password']
+        ).first()
+
+        if user:
+            session['user'] = user.username
+            session['role'] = user.role
+            return redirect('/')
+    return '''
+    <form method="post">
+        <input name="username" placeholder="Username">
+        <input name="password" placeholder="Password">
+        <button>Login</button>
+    </form>
+    '''
+
+# SIGNUP (ADMIN CAN CREATE USERS)
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        user = User(
+            username=request.form['username'],
+            password=request.form['password'],
+            role="user"
+        )
+        db.session.add(user)
+        db.session.commit()
         return redirect('/login')
 
-    user_tickets = [t for t in tickets if t['user_id'] == session['user_id']]
+    return '''
+    <form method="post">
+        <input name="username">
+        <input name="password">
+        <button>Create User</button>
+    </form>
+    '''
 
-    return render_template('tickets.html', tickets=user_tickets)
-
-# IMPORTANT for Render
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
