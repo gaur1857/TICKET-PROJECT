ed34398011cf3e1d3e264d647a44999ebe6cbc0f
gaur1857 <gaur.1857@gmail.com>
2026-04-17 14:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__)
+
+tickets = []
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/add', methods=['POST'])
+def add_ticket():
+    name = request.form['name']
+    issue = request.form['issue']
+
+    tickets.append({'name': name, 'issue': issue})
+    return redirect('/tickets')
+
 @app.route('/tickets')
-def tickets():
-    return render_template('tickets.html')
+def view_tickets():
+    return render_template('tickets.html', tickets=tickets)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
